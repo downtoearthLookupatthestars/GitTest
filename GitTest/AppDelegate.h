cfbf6d7f0f77cef3ef3bcac33609431d8ad4bfa0
Mr Luo <Mr.Luo@MrLuodeMacBook-Pro.local>
2019-03-07 16:40:54 +0800

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Mr.Luo on 2019/3/7.
+//  Copyright © 2019 com.Sten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Mr.Luo on 2019/3/7.
+//  Copyright © 2019 com.Sten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
