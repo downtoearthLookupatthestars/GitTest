39c9d3096cdc4849515f72e1af6a68ab92a738b3
Mr Luo <Mr.Luo@MrLuodeMacBook-Pro.local>
2019-03-08 13:54:07 +0800

    第三次更新

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"hello world");
+    NSLog(@"nihao");
     // Do any additional setup after loading the view, typically from a nib.
 }
 @end
